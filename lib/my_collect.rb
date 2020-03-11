879fd84267303c6f2deb73dd7916658f35881dbb
Adam Priest <sepone229@gmail.com>
2020-03-11 17:36:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,10 @@ def my_collect(array)
   array = []
   i = 0
   while i < array.length
+    array << yield(array[i])
     i += 1
-    my_collect(array) do |word|
-      puts word.upcase
-    end
   end
+  array
 end
+
+my_collect(array) = 
