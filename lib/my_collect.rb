c92b0d73c114061807df1a4582e4a8d3cd0d706a
Adam Priest <sepone229@gmail.com>
2020-03-11 17:38:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,10 @@ def my_collect(array)
   new_array = []
   i = 0
   while i < new_array.length
-    array << yield(new_array[i])
+    new_array << yield(new_array[i])
     i += 1
   end
-  array
+  new_array
 end
 
-my_collect(array) {|i| i.split(" ").first}
+my_collect(new_array) {|i| i.split(" ").first}
