13387f5d700a842fef5a0424c2e56b79d6d2c8d7
Adam Priest <sepone229@gmail.com>
2020-03-11 17:27:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  array = []
+  i = 0
+  while i < array.length
+    array << yield(array[i])
+    i += 1
+  end
+  my_collect(array) do |name|
+    name.upcase
+  end
+end
